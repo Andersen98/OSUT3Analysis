dd578626d42b62e4cce6b4ea2a727a827dc29777
Ethan Andersen <thehumanme@gmail.com>
2017-10-30 15:12:01 -0400

    Made a sub argument set for fermilab case

--- DBTools/python/condorSubArgumentsSet.py
+++ DBTools/python/condorSubArgumentsSet.py
@@ -12,3 +12,18 @@ CondorSubArgumentsSet = {
     11  :  {'+IsLocalJob'            :  'true'},
     12  :  {'Rank'                   :  'TARGET.IsLocalSlot\n'},
 }
+
+CondorSubArgumentsSet_Fermi = {
+    1   :  {'Executable'             :  ''},
+    2   :  {'Universe'               :  'vanilla'},
+    3   :  {'Getenv'                 :  'True'},
+    4   :  {'request_memory '        : '2048MB'},
+    5   :  {'Arguments'              :  ''},
+    6   :  {'Output'                 :  'condor_$(Process).out'},
+    7   :  {'Error'                  :  'condor_$(Process).err'},
+    8   :  {'Log'                    :  'condor_$(Process).log\n'},
+    9   :  {'Transfer_Input_files'   :  ''},
+    10  :  {'Transfer_Output_files'  :  ''},
+    11  :  {'+IsLocalJob'            :  'true'},
+    12  :  {'Rank'                   :  'TARGET.IsLocalSlot\n'},
+}
